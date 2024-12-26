69cb132089b88334fbd0174c3f9d017e8b6ae84a
Tendaman <122923185+Tendaman@users.noreply.github.com>
2024-12-26 14:06:46 +0200

    uploadthing

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true,   // Ignores TypeScript errors during production builds
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true,  // Ignores ESLint errors during production builds
+    ignoreDuringBuilds: true, 
   },
   images: {
     domains: [
